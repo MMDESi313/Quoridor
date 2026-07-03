d42ed76c48bf5d0faff3d5ca0a4959f283e0de12
MMD ESi <moeil985.51fk@gmail.com>
2026-07-03 22:49:31 +0330

    feat: implement 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 function NotFoundPage() {
   return (
-    <main>
-      <h1 className="text-xl font-black">۴۰۴</h1>
+    <main className="h-dvh flex">
+      <div className="h-full w-1/2 bg-[#0000FF25] absolute -z-10 right-0" />
+      <div className="h-full w-1/2 bg-[#FF000025] absolute -z-10 left-0" />
+
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <h1 className="text-9xl font-bold text-[#FF0000]">۴۰۴</h1>
+        <p className="font-semibold text-[#0000FF] text-xl">
+          صفحه مورد نظر وجود ندارد!
+        </p>
+        <Link
+          to={"/"}
+          className="text-white text-xl bg-[#0000FF] py-2 px-8 mt-8 rounded-xl"
+        >
+          خانه
+        </Link>
+      </div>
     </main>
   );
 }
